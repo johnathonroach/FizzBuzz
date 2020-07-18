ebaac3eeca9982bf3bd70b81cbf384376004fa3e
Johnathon Roach <johnathon.roach@gmail.com>
2020-07-17 23:50:39 -0500

    Add vue js to the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,49 +1,32 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
 
-(function(){
+window.Vue = require('vue');
 
-    console.log('aaaand in JavaScript...');
-    console.log(`%c
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
 
-        FFFFFFFFFFFFFFFFFFFFFF  iiii                                    BBBBBBBBBBBBBBBBB                                                       
-        F::::::::::::::::::::F i::::i                                   B::::::::::::::::B                                                      
-        F::::::::::::::::::::F  iiii                                    B::::::BBBBBB:::::B                                                     
-        FF::::::FFFFFFFFF::::F                                          BB:::::B     B:::::B                                                    
-          F:::::F       FFFFFFiiiiiii zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz  B::::B     B:::::Buuuuuu    uuuuuu  zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-          F:::::F             i:::::i z:::::::::::::::zz:::::::::::::::z  B::::B     B:::::Bu::::u    u::::u  z:::::::::::::::zz:::::::::::::::z
-          F::::::FFFFFFFFFF    i::::i z::::::::::::::z z::::::::::::::z   B::::BBBBBB:::::B u::::u    u::::u  z::::::::::::::z z::::::::::::::z 
-          F:::::::::::::::F    i::::i zzzzzzzz::::::z  zzzzzzzz::::::z    B:::::::::::::BB  u::::u    u::::u  zzzzzzzz::::::z  zzzzzzzz::::::z  
-          F:::::::::::::::F    i::::i       z::::::z         z::::::z     B::::BBBBBB:::::B u::::u    u::::u        z::::::z         z::::::z   
-          F::::::FFFFFFFFFF    i::::i      z::::::z         z::::::z      B::::B     B:::::Bu::::u    u::::u       z::::::z         z::::::z    
-          F:::::F              i::::i     z::::::z         z::::::z       B::::B     B:::::Bu::::u    u::::u      z::::::z         z::::::z     
-          F:::::F              i::::i    z::::::z         z::::::z        B::::B     B:::::Bu:::::uuuu:::::u     z::::::z         z::::::z      
-        FF:::::::FF           i::::::i  z::::::zzzzzzzz  z::::::zzzzzzzzBB:::::BBBBBB::::::Bu:::::::::::::::uu  z::::::zzzzzzzz  z::::::zzzzzzzz
-        F::::::::FF           i::::::i z::::::::::::::z z::::::::::::::zB:::::::::::::::::B  u:::::::::::::::u z::::::::::::::z z::::::::::::::z
-        F::::::::FF           i::::::iz:::::::::::::::zz:::::::::::::::zB::::::::::::::::B    uu::::::::uu:::uz:::::::::::::::zz:::::::::::::::z
-        FFFFFFFFFFF           iiiiiiiizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzBBBBBBBBBBBBBBBBB       uuuuuuuu  uuuuzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-                                                                                                                                                
-    `, "font-family:monospace")
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-    function isDivisibleBy3($number){
-        return $number % 3 === 0;
-    }
-    
-    function isDivisibleBy5($number){
-        return $number % 5 === 0;
-    }
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-  
-    for(let i = 1; i < 101; i++){
-        if(isDivisibleBy3(i) && isDivisibleBy5(i)){
-            console.log('%cFizzBuzz ', 'color: #0033bb');
-        } else if(isDivisibleBy3(i)){
-            console.log('%cFizz ', 'color: #00aabb');
-        }else if(isDivisibleBy5(i)){
-            console.log('%cBuzz ', 'color: #aa00bb');
-        } else{
-            console.log('%c'+i, 'color: #333');
-        }
-    }
-    
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
 
-})();
+const app = new Vue({
+    el: '#app',
+});
